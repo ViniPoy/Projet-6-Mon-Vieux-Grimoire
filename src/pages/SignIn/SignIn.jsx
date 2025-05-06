7c39dd225ff5ad808c421ac7af535375e5317d4f
ViniPoy <vinipoy@gmail.com>
2025-05-06 11:38:45 +0200

    Ajout d'un message d'erreur pour éviter aux utilisateur de voir le message: error 400, bad request

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -64,7 +64,7 @@ function SignIn({ setUser }) {
       }
       setNotification({ error: false, message: 'Votre compte a bien été créé, vous pouvez vous connecter' });
     } catch (err) {
-      setNotification({ error: true, message: err.message });
+      setNotification({ error: true, message: 'Ce compte éxiste déjà, veuillez vous connectés' });
       console.log('Some error occured during signing up: ', err);
     } finally {
       setIsLoading(false);
